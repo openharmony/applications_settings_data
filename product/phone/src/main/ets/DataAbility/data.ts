ceec300720d241cea7121b21050c5b3a9a755c28
weiyunxing <weiyunxing@huawei.com>
2022-09-20 10:10:00 +0800

    日志格式修改&增加权限校验
    Signed-off-by: weiyunxing <weiyunxing@huawei.com>

--- product/phone/src/main/ets/DataAbility/data.ts
+++ product/phone/src/main/ets/DataAbility/data.ts
@@ -14,82 +14,119 @@
  * limitations under the License.
  */
 import Audio from '@ohos.multimedia.audio';
-import Brightness from '@ohos.brightness';
 import bluetooth from '@ohos.bluetooth';
-import dataAbility from '@ohos.data.dataability';
+import dataAbility from '@ohos.data.dataAbility';
 import featureAbility from '@ohos.ability.featureAbility';
 import settings from '@ohos.settings';
 import SettingsDataConfig from '../Utils/SettingsDataConfig';
 import SettingsDBHelper from '../Utils/SettingsDBHelper';
+import { Log } from '../Utils/Log';
 
-let TAG = 'SettingsData ';
+import rpc from '@ohos.rpc';
+import abilityAccessCtrl from '@ohos.abilityAccessCtrl';
+
 let rdbStore;
-let requests:any[] = [];
+let requests: any[] = [];
 
-function DoSystemSetting(settingsKey : string, settingsValue : string) {
+function DoSystemSetting(settingsKey: string, settingsValue: string) {
     switch (settingsKey) {
-        case SettingsDataConfig.SettingsKey.SETTINGS_AUDIO_RINGTONE:
-        {
+        case SettingsDataConfig.SettingsKey.SETTINGS_AUDIO_RINGTONE: {
             try {
                 let volumeType = Audio.AudioVolumeType.RINGTONE;
-                console.log(TAG + 'settings RINGTONE start')
+                Log.I('settings RINGTONE start')
                 Audio.getAudioManager().setVolume(volumeType, parseInt(settingsValue)).then(() => {
-                    console.log(TAG + 'settings Promise returned to indicate a successful RINGTONE setting.')
+                    Log.I('settings Promise returned to indicate a successful RINGTONE setting.')
                 });
             } catch (err) {
-                console.log(TAG + 'settings RINGTONE failed error = ' + JSON.stringify(err));
+                Log.I('settings RINGTONE failed error = ' + JSON.stringify(err));
             }
-        }break
-        case SettingsDataConfig.SettingsKey.SETTINGS_AUDIO_MEDIA:
-        {
+            break
+        }
+        case SettingsDataConfig.SettingsKey.SETTINGS_AUDIO_MEDIA: {
             try {
                 let volumeType = Audio.AudioVolumeType.MEDIA;
-                console.log(TAG + 'settings MEDIA start')
+                Log.I('settings MEDIA start')
                 Audio.getAudioManager().setVolume(volumeType, parseInt(settingsValue)).then(() => {
-                    console.log(TAG + 'settings Promise returned to indicate a successful MEDIA setting.')
+                    Log.I('settings Promise returned to indicate a successful MEDIA setting.')
                 });
             } catch (err) {
-                console.log(TAG + 'settings MEDIA failed error = ' + JSON.stringify(err));
+                Log.I('settings MEDIA failed error = ' + JSON.stringify(err));
             }
-        }break
-        case SettingsDataConfig.SettingsKey.SETTINGS_AUDIO_VOICE_CALL:
-        {
+            break
+        }
+        case SettingsDataConfig.SettingsKey.SETTINGS_AUDIO_VOICE_CALL: {
             try {
                 let volumeType = Audio.AudioVolumeType.VOICE_CALL;
-                console.log(TAG + 'settings VOICE_CALL start')
+                Log.I('settings VOICE_CALL start')
                 Audio.getAudioManager().setVolume(volumeType, parseInt(settingsValue)).then(() => {
-                    console.log(TAG + 'settings Promise returned to indicate a successful VOICE_CALL setting.')
+                    Log.I('settings Promise returned to indicate a successful VOICE_CALL setting.')
                 });
             } catch (err) {
-                console.log(TAG + 'settings VOICE_CALL failed error = ' + JSON.stringify(err));
+                Log.I('settings VOICE_CALL failed error = ' + JSON.stringify(err));
             }
-        }break
-        case settings.general.DEVICE_NAME:
-        {
+            break
+        }
+        case settings.general.DEVICE_NAME: {
             try {
                 let result = bluetooth.setLocalName(settingsValue);
-                console.log(TAG + 'settings bluetooth_name res = ' + result)
+                Log.I('settings bluetooth_name res = ' + result)
             } catch (err) {
-                console.log(TAG + 'settings bluetooth_name failed, error = ' + JSON.stringify(err));
+                Log.I('settings bluetooth_name failed, error = ' + JSON.stringify(err));
             }
-        }break
-        default :
-            {
-                console.log(TAG + settingsKey+' key is not brightness or audio or deviceName');
-            }
+            break
+        }
+        default: {
+            Log.I(settingsKey + ' key is not brightness or audio or deviceName');
+        }
     }
+}
+
+function verifyPermission(callBack) {
+    try {
+        let tokenID = rpc.IPCSkeleton.getCallingTokenId();
+        Log.I('tokenID = ' + tokenID);
+        let grantStatus = abilityAccessCtrl.createAtManager().verifyAccessToken(tokenID, "ohos.permission.MANAGE_SECURE_SETTINGS");
+        grantStatus.then(data => {
+            if (data == abilityAccessCtrl.GrantStatus.PERMISSION_GRANTED) {
+                Log.I('MANAGE_SECURE_SETTINGS active');
+                callBack(true);
+            } else {
+                Log.W('MANAGE_SECURE_SETTINGS grantStatus= ' + JSON.stringify(data));
+                callBack(false);
+            }
+        }).catch((err) => {
+            Log.E('tokenID = ' + tokenID + ' verifyAccessToken is failed: ' + JSON.stringify(err));
+            callBack(false);
+        })
+    } catch (err) {
+        Log.E('err = ' + JSON.stringify(err));
+        callBack(false);
+    }
+}
+
+function isTrustList(keyWord: String) {
+    let ret = -1;
+    let trustList: Array<String> = [
+        settings.display.SCREEN_BRIGHTNESS_STATUS,
+        settings.display.AUTO_SCREEN_BRIGHTNESS,
+        settings.display.SCREEN_OFF_TIMEOUT
+    ];
+    ret = trustList.findIndex((trustItem) => {
+        return trustItem === keyWord
+    })
+    return ret != -1;
 }
 
 export default {
     onInitialized(abilityInfo) {
-        console.log(TAG + 'onInitialize getContext start');
+        Log.I('onInitialize getContext start');
         let context = featureAbility.getContext();
         globalThis.abilityContext = context;
-        if(context != null) {
+        if (context != null) {
             SettingsDBHelper.getInstance().getRdbStore().then((rdb: any) => {
-                console.log(TAG + " onInitialized rdb：" + rdb);
+                Log.I(" onInitialized rdb：" + rdb);
                 rdbStore = rdb;
-                console.log(TAG + " onInitialized requests：" + JSON.stringify(requests));
+                Log.I(" onInitialized requests：" + JSON.stringify(requests));
                 for (let i = 0; i < requests.length; i++) {
                     let opt: string = requests[i]["operation"];
                     let columns = requests[i]["columns"];
@@ -97,82 +134,127 @@ export default {
                     let value = requests[i]["value"];
                     if (opt == "insert") {
                         rdbStore.insert(SettingsDataConfig.TABLE_NAME, value, function (err, ret) {
-                            console.log(TAG + 'onInitialized insert ret: ' + ret);
+                            Log.I('onInitialized insert ret: ' + ret);
                         });
                     } else if (opt == "query") {
                         let rdbPredicates = dataAbility.createRdbPredicates(SettingsDataConfig.TABLE_NAME, predicates);
                         rdbStore.query(rdbPredicates, columns, function (err, resultSet) {
-                            console.log(TAG + 'onInitialized query ret: ' + JSON.stringify(resultSet));
+                            Log.I('onInitialized query ret: ' + JSON.stringify(resultSet));
                         });
                     } else if (opt == "update") {
                         let rdbPredicates = dataAbility.createRdbPredicates(SettingsDataConfig.TABLE_NAME, predicates);
                         rdbStore.update(value, rdbPredicates, function (err, ret) {
-                            console.log(TAG + 'onInitialized update ret: ' + ret);
+                            Log.I('onInitialized update ret: ' + ret);
                         });
                     }
                 }
             }).catch(err => {
-                console.log(TAG + 'onInitialize failed');
+                Log.I('onInitialize failed');
             })
-            console.log(TAG + 'onInitialize end');
+            Log.I('onInitialize end');
         } else {
-            console.log(TAG + 'onInitialize context error!');
+            Log.I('onInitialize context error!');
         }
     },
 
     insert(uri, value, callback) {
-        console.log(TAG + 'insert keyword = ' + value["KEYWORD"] + ' start:' + uri);
-        DoSystemSetting(value["KEYWORD"], value["VALUE"]);
-        if (rdbStore == null) {
-            let request= {"operation":"insert", "columns" : null, "predicates" : null, value:value};
-            console.log(TAG + 'insert request = '+ JSON.stringify(request));
-            requests.push(request);
+        Log.I('insert keyword = ' + value["KEYWORD"] + ' start:' + uri);
+        let rdbInsert = (GrantStatus) => {
+            if (!GrantStatus) {
+                callback(-1, 0);
+                return;
+            }
+            DoSystemSetting(value["KEYWORD"], value["VALUE"]);
+            if (rdbStore == null) {
+                let request = {
+                    "operation": "insert", "columns": null, "predicates": null, value: value
+                };
+                Log.I('insert request = ' + JSON.stringify(request));
+                requests.push(request);
+                callback(-1, 0);
+            } else {
+                rdbStore.insert(SettingsDataConfig.TABLE_NAME, value, function (err, ret) {
+                    Log.I('insert before callback ');
+                    callback(err, ret);
+                    Log.I('insert after callback ');
+                    Log.I('insert result: ' + ret);
+                });
+            }
+        }
+
+        try {
+            if (isTrustList(value["KEYWORD"])) {
+                Log.I('trustList data exists.');
+                rdbInsert(true);
+            } else {
+                Log.I('Start to verify permissions.');
+                verifyPermission(rdbInsert);
+            }
+        } catch (err) {
+            Log.E('Insert Data error:' + JSON.stringify(err));
             callback(-1, 0);
-        } else {
-            rdbStore.insert(SettingsDataConfig.TABLE_NAME, value, function (err, ret) {
-                console.log(TAG + 'insert before callback ');
-                callback(err, ret);
-                console.log(TAG + 'insert after callback ');
-                console.log(TAG + 'insert result: ' + ret);
-            });
         }
     },
 
     query(uri, columns, predicates, callback) {
-        console.log(TAG + 'query start uri:' + uri);
+        Log.I('query start uri:' + uri);
         if (rdbStore == null) {
-            let request= {"operation":"query", "columns" : columns, "predicates" : predicates, value:""};
-            console.log(TAG + 'query request = '+ JSON.stringify(request));
+            let request = {
+                "operation": "query", "columns": columns, "predicates": predicates, value: ""
+            };
+            Log.I('query request = ' + JSON.stringify(request));
             requests.push(request);
-            callback(-1, {"_napiwrapper":{}});
+            callback(-1, {
+                "_napiwrapper": {}
+            });
         } else {
             let rdbPredicates = dataAbility.createRdbPredicates(SettingsDataConfig.TABLE_NAME, predicates);
             rdbStore.query(rdbPredicates, columns, function (err, resultSet) {
-                console.log(TAG + 'query before callback err:' + JSON.stringify(err) + " ResultSet" + JSON.stringify(resultSet));
+                Log.I('query before callback err:' + JSON.stringify(err) + " ResultSet" + JSON.stringify(resultSet));
                 callback(err, resultSet);
-                console.log(TAG + 'query after callback ');
-                console.log(TAG + 'query result: '+ JSON.stringify(resultSet));
+                Log.I('query after callback ');
+                Log.I('query result: ' + JSON.stringify(resultSet));
             });
         }
     },
 
     update(uri, value, predicates, callback) {
-        console.log(TAG + 'update keyword = ' + value["KEYWORD"] + ' start:' + uri);
-        DoSystemSetting(value["KEYWORD"], value["VALUE"]);
-        if (rdbStore == null) {
-            let request = {"operation":"update", "columns" : null, "predicates" : predicates, value : value};
-            console.log(TAG + 'update request = '+ JSON.stringify(request));
-            requests.push(request);
-            callback(-1, 0);
-        } else {
-            let rdbPredicates = dataAbility.createRdbPredicates(SettingsDataConfig.TABLE_NAME, predicates);
-            rdbStore.update(value, rdbPredicates, function (err, ret) {
-                console.log(TAG + 'update before callback ' + JSON.stringify(err));
-                callback(err, ret);
-                console.log(TAG + 'update after callback ' + JSON.stringify(ret));
-                console.log(TAG + 'update result: ' + ret);
-            });
+        Log.I('update keyword = ' + value["KEYWORD"] + ' start:' + uri);
+        let rdbUpData = (GrantStatus) => {
+            if (!GrantStatus) {
+                callback(-1, 0);
+                return;
+            }
+            DoSystemSetting(value["KEYWORD"], value["VALUE"]);
+            if (rdbStore == null) {
+                let request = {
+                    "operation": "update", "columns": null, "predicates": predicates, value: value
+                };
+                Log.I('update request = ' + JSON.stringify(request));
+                requests.push(request);
+                callback(-1, 0);
+            } else {
+                let rdbPredicates = dataAbility.createRdbPredicates(SettingsDataConfig.TABLE_NAME, predicates);
+                rdbStore.update(value, rdbPredicates, function (err, ret) {
+                    Log.I('update before callback ' + JSON.stringify(err));
+                    callback(err, ret);
+                    Log.I('update after callback ' + JSON.stringify(ret));
+                    Log.I('update result: ' + ret);
+                });
+            }
         }
 
+        try {
+            if (isTrustList(value["KEYWORD"])) {
+                Log.I('trustList data exists.');
+                rdbUpData(true);
+            } else {
+                Log.I('Start to verify permissions.');
+                verifyPermission(rdbUpData);
+            }
+        } catch (err) {
+            Log.E('upData error:' + JSON.stringify(err));
+            callback(-1, 0);
+        }
     }
 };
