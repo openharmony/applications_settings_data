cbfc234f073ac0751331fa55fa1972a1aac32504
liminjie10 <liminjie10@huawei.com>
2023-02-21 10:31:58 +0800

    SettingsData支持多用户隔离
    Signed-off-by: liminjie10 <liminjie10@huawei.com>

---
+++ entry/src/main/ets/Application/DataAbilityStage.ts
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2023-2023. All rights reserved.
+ */
+
+import { Log } from '../Utils/Log';
+import AbilityStage from "@ohos.app.ability.AbilityStage"
+
+export default class DataAbilityStage extends AbilityStage {
+    onCreate() {
+        Log.I('DataAbilityStage onCreate');
+        globalThis.abilityContext = this.context;
+    }
+}
